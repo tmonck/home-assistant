ff4dc327333e1aa91e2a75069cca843d428215e2
Tom Monck <tom@monck.info>
2022-12-19 15:15:04 -0500

    Remove logger per feedback

--- homeassistant/components/moonraker/__init__.py
+++ homeassistant/components/moonraker/__init__.py
@@ -1,8 +1,6 @@
 """The moonraker integration."""
 from __future__ import annotations
 
-import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -10,8 +8,6 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .connector import APIConnector
 from .const import DATA_CONNECTOR, DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = [Platform.SENSOR]
 
